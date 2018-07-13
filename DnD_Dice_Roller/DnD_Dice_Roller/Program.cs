decd52f66335c71d0f1130ed3129efdc5d466fa4
Ryan Staudacher <ryanmstaudacher@gmail.com>
2018-07-13 01:22:31 -0500

    Seperates each line and gives sum for each as well as total sum at the end.

--- DnD_Dice_Roller/DnD_Dice_Roller/Program.cs
+++ DnD_Dice_Roller/DnD_Dice_Roller/Program.cs
@@ -7,37 +7,55 @@ namespace DnD_Dice_Roller
 {
     class Program
     {
-        static void Main(string[] args)
+        private static string line;
+
+        private static void Main(string[] args)
         {
-            int sumNumber = 0;
-            string line;
-            string allNumbers = "";
+            DiceRoller();
+        }
+
+        private static void DiceRoller()
+        {
+            List<List<int>> dieRolls = new List<List<int>>();
             Random r = new Random();
-            List<int> dieRolls = new List<int>();
+            string allNumbers = "";
+            int totalSum = 0;
 
             Console.WriteLine("Please type out which dice to roll, with each type of die being on its own line. " +
                 "Use the format 'NdM', where N = number of dice to roll and M = type of die. \n\nExample: \n2d4\n3d6\n5d12\n\nType 'roll' on a new line when you are ready to roll the dice!");
 
-            while((line = Console.ReadLine()) != "roll")
+            // While the user has not entered 'roll' on a new line
+            while ((line = Console.ReadLine()) != "roll")
             {
                 int dIndex;
                 string firstNumberString;
                 string secondNumberString;
 
-                if(line.IndexOf('d') >= 1)
+                // Detects if there is a 'd' somewhere on the current line. If not, output error message.
+                if (line.IndexOf('d') >= 1)
                 {
+                    // Gets the 'd' ;)
                     dIndex = line.IndexOf('d');
+                    // Gets the string of the number that is before the 'd'
                     firstNumberString = line.Substring(0, dIndex);
+                    // Gets the string of the number that is after the 'd'
                     secondNumberString = line.Substring(dIndex + 1);
 
+                    // If the two substrings are able to be parsed into two numbers, do something, if not output error message
                     if (Int32.TryParse(firstNumberString, out int firstNumber) && Int32.TryParse(secondNumberString, out int secondNumber))
                     {
+                        List<int> l = new List<int>();
+
+                        // Roll each die and add it to the sublist
                         for (int i = 0; i < firstNumber; i++)
                         {
                             int newRoll = r.Next(1, secondNumber + 1);
 
-                            dieRolls.Add(newRoll);
+                            l.Add(newRoll);
                         }
+
+                        // Add the sublist to the dieRolls list
+                        dieRolls.Add(l);
                     }
                     else
                     {
@@ -50,24 +68,36 @@ namespace DnD_Dice_Roller
                 }
             }
 
-            for (int i = 0; i < dieRolls.Count; i++)
+            // For aestethic reasons :D
+            Console.WriteLine("");
+            
+            foreach (List<int> l in dieRolls)
             {
-                sumNumber += dieRolls[i];
+                int sumNumber = 0;
+                string allNumbersInList = "";
+
+                // Takes each roll in each sublist and adds them together to get the sum.
+                // Takes each roll in each sublist and adds it to a string so the user can see what each roll was.
+                for (int i = 0; i < l.Count; i++)
+                {
+                    sumNumber += l[i];
+                    totalSum += l[i];
+                    allNumbers += l[i] + ", ";
+
+                    if (i == (l.Count - 1))
+                    {
+                        allNumbersInList += l[i] + "";
+                    }
+                    else
+                    {
+                        allNumbersInList += l[i] + ", ";
+                    }
+                }
+
+                Console.WriteLine(sumNumber + ": " + allNumbersInList);
             }
 
-            for (int i = 0; i < dieRolls.Count; i++)
-            {
-                if(i == (dieRolls.Count - 1))
-                {
-					allNumbers += dieRolls[i] + "";
-                }
-                else
-                {
-                    allNumbers += dieRolls[i] + ", ";
-                }
-            }
-
-            Console.WriteLine(sumNumber + ": " + allNumbers);
+            Console.WriteLine("\nTotal - " + totalSum + ": " + allNumbers);
 
             Console.ReadKey();
         }
